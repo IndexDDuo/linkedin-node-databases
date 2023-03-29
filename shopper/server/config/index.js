9fac6d1667daee4bedff15f70132b19174bc4e6b
Jing Li <jl.jing02123@gmail.com>
2023-03-28 22:05:55 -0400

    commit

--- shopper/server/config/index.js
+++ shopper/server/config/index.js
@@ -1,8 +1,8 @@
-const pkg = require('../../package.json');
+const pkg = require("../../package.json");
 
 module.exports = {
   applicationName: pkg.name,
   mongodb: {
-    url: 'mongodb://localhost:37017/shopper',
-  }
+    url: "mongodb://localhost:37017/shopper",
+  },
 };
